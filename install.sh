c463ab331f2db6fedf85a3f58a61d0647f0e333e
Glenn Morton <glenn@sandcastle.io>
2015-04-03 10:10:38 +0800

    Added clean up step

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 # change default shell to zsh
 chsh -s /bin/zsh
 
-# create clone folder
+# fir
 dir="$HOME/dev/.files"
+
+# remove old version
+rm -rf /Users/glenn/dev/.files
+
+# create clone folder
 mkdir -p $dir
-cd $dir
 
 # clone repo
 git clone --recursive git://github.com/sandcastle/dotfiles.git $dir
-cd dotfiles
 
 # symlink files
 sudo sh "$dir/symlink-dotfiles.sh"
