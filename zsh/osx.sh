9d761a53cb1387df649d79a94e76440028a20c01
Glenn Morton <glenn@sandcastle.io>
2015-08-08 15:52:15 +0800

    aliases for desktop icons

--- zsh/osx.sh
+++ zsh/osx.sh
@@ -27,3 +27,7 @@ alias pgrep='pgrep -fli'
 # Hide / show files
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+# Hide/show all desktop icons (useful when presenting)
+alias showdeskicons="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+alias hidedeskicons="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
