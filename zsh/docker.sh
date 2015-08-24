631365b35b0b1be00d7df47d3a012c4f8159aa5f
Glenn Morton <glenn@sandcastle.io>
2015-08-24 17:18:21 +1000

    More aliases for docker compose

--- zsh/docker.sh
+++ zsh/docker.sh
@@ -7,5 +7,10 @@
 # Docker
 alias dk='docker'
 alias dkm='docker-machine'
-alias dkc='docker-compose'
 alias dks='docker-swarm'
+
+# Docker Compose
+alias dkc='docker-compose'
+alias dkcu='docker-compose up'
+alias dkcd='docker-compose up -d'
+alias dkcs='docker-compose stop'
