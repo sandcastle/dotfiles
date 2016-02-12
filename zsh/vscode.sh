da11504c359b3b510a376a5a4104abee919ada76
Glenn Morton <glenn@sandcastle.io>
2016-02-12 15:14:12 +0800

    Enable salsa for vscode

--- zsh/vscode.sh
+++ zsh/vscode.sh
@@ -4,5 +4,7 @@
 # Visual Studio Code
 # ==================================================================
 
+# enable Salsa engine (https://code.visualstudio.com/updates#vscode)
+export VSCODE_TSJS=1
 
 code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* }
