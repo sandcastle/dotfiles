6dcfcda064d52467286223a34876e4a5b0f8f235
Glenn Morton <glenn@sandcastle.io>
2016-05-24 13:27:29 +0800

    updated cask tool stack

--- bootstrap/tools.sh
+++ bootstrap/tools.sh
@@ -12,8 +12,20 @@ brew cask install qlcolorcode
 brew cask install qlimagesize
 brew cask install webpquicklook
 
-# dev doco
+# general
+brew cask install flux
+brew cask install ccleaner
+brew cask install rescuetime
+brew cask install screenflow
+brew cask install expandrive
+brew cask install TimeMachineScheduler
+
+# dev
+brew cask install atom
 brew cask install dash
+brew cask install colour-contrast-analyser
+brew cask install github-desktop
+brew cask install terraform
 
 # security
 brew cask install 1password
