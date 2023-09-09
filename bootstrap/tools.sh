b05f3772942a324e8e34e75a00a2456f904c00a2
Glenn Morton <glenn@sandcastle.io>
2023-09-09 17:47:09 +0800

    feat: Add bun install

--- bootstrap/tools.sh
+++ bootstrap/tools.sh
@@ -84,6 +84,9 @@ brew install jq
 brew install iterm2
 curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
 
+#bun
+curl -fsSL https://bun.sh/install | bash
+
 # git
 brew install github
 brew install gitkraken
