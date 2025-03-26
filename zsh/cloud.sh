f25caa35745973b7b7039b2286c3da79bbe531d7
Glenn Morton <glenn.morton@komo.tech>
2025-03-26 16:55:25 +0800

    feat: gcp default app creds

--- zsh/cloud.sh
+++ zsh/cloud.sh
@@ -4,6 +4,10 @@
 # Google Cloud
 # ==================================================================
 
+[ -f ~/.config/gcloud/application_default_credentials.json ] && \
+  export GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/application_default_credentials.json
+
+
 # CLI mapping for py version
 export CLOUDSDK_PYTHON=python3
 source "$(gcloud info --format='value(installation.sdk_root)')/path.zsh.inc"
