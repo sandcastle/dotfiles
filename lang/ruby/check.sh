bd5d0eaeea52b347ef7bdc183922580e12589297
Glenn Morton <glenn.morton@komo.tech>
2025-07-18 21:57:56 +0800

    fix: Improve ruby env

--- lang/ruby/check.sh
+++ lang/ruby/check.sh
@@ -7,6 +7,6 @@ for gem in "${INSTALL_GEMS[@]}"; do
   if ! gem list "$gem" -i >/dev/null 2>&1; then
     echo "Installing gem: $gem"
     gem install "$gem"
-    rbenv rehash
+    mise reshim
   fi
 done
