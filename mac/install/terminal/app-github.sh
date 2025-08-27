cad6c3117138e0cdd6efddcef8f653482414770a
Glenn Morton <glenn.morton@komo.tech>
2025-08-27 10:55:27 +0800

    app: Github CLI for mac

---
+++ mac/install/terminal/app-github.sh
@@ -0,0 +1,6 @@
+
+
+brew install gh
+
+# Sub-issue extensions not supported by gh currently
+gh extension install yahsan2/gh-sub-issue
---
+++ mac/uninstall/app-github.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# GitHub CLI
+# https://cli.github.com/
+
+brew uninstall gh
