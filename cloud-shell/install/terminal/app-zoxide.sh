1edc896734f0659183b2e15f89828a55703e60ea
Glenn Morton <glenn.morton@komo.tech>
2025-11-03 16:39:05 +0800

    fix: More cloudshell updates

---
+++ cloud-shell/install/terminal/app-zoxide.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Zoxide
+# https://github.com/ajeetdsouza/zoxide
+echo "Installing Zoxide..."
+
+# Install zoxide
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
