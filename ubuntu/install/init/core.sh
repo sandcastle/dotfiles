3aac8276e4cd4df06b95c00b3df6b00b3b7b7e17
Glenn Morton <glenn.morton@komo.tech>
2025-12-12 14:58:17 +0800

    feat: Improve core tools on linux

---
+++ ubuntu/install/init/core.sh
@@ -0,0 +1,23 @@
+sudo apt update -y
+
+# Core tools
+sudo apt install -y \
+  curl \
+  wget \
+  git \
+  bash-completion
+
+# Modern CLI tools
+sudo apt install -y \
+  zsh \
+  starship \
+  zoxide \
+  fzf \
+  ripgrep \
+  bat \
+  exa \
+  delta \
+  fd \
+  du-dust \
+  hyperfine \
+  pwgen
---
+++ ubuntu/uninstall/init/core.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Core tools
+sudo apt uninstall -y \
+  curl \
+  wget \
+  git \
+  bash-completion \
+  zsh \
+  starship \
+  zoxide \
+  fzf \
+  ripgrep \
+  bat \
+  exa \
+  delta \
+  fd \
+  du-dust \
+  hyperfine \
+  pwgen
