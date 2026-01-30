a976e60d758b38c84259ae87b6fc1ba0fb0a87a8
Glenn Morton <glenn.morton@komo.tech>
2026-01-30 09:23:49 +0800

    feat: Brew auto-update

--- mac/install/init/app-brew.sh
+++ mac/install/init/app-brew.sh
@@ -5,3 +5,12 @@ if ! command -v brew >/dev/null; then
   # MacOS comes with bash (old) and curl
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+# Install Homebrew Autoupdate
+brew tap domt4/autoupdate
+
+# Required for sudo (casks that need sudo)
+brew install pinentry-mac
+
+# Schedule for every hour to upgrade and cleanup
+brew autoupdate start 3600 --upgrade --cleanup --sudo
